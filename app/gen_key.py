22ce522d4de9dbb457794df85673ba464cee2aaa
Danail Vankov (DVAN) <Danail.Vankov@softwareag.com>
2025-02-24 14:41:15 +0000

    Initial commit with MetaModule and API server

--- app/gen_key.py
+++
@@ -1,7 +0,0 @@
-import secrets
-
-# Генериране на 32-байтов API ключ
-api_key = secrets.token_hex(16)  # Генерира 32-символен ключ
-print("API Key:", api_key)
-with open('api_key.txt', 'w') as file:
-    file.write(api_key)
